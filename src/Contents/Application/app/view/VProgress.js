81c163750c08582ef3adae6e6ffd08a3209b89a2
Stéphane Zucatti <stephane.zucatti@me.com>
2015-05-20 11:32:04 +0200

    Update# 2

--- src/Contents/Application/app/view/VProgress.js
+++ src/Contents/Application/app/view/VProgress.js
@@ -1,39 +1,16 @@
-App.view.define('VImport',{
+App.view.define('VProgress',{
 	extend: "Ext.window.Window",
-    alias: "widget.TImport",
+    alias: "widget.TProgress",
 	initComponent: function() {
 		this.width = 600;
-        this.height = 250;
-        this.title = "Importer un fichier d'acquisition";
+        this.height = 80;
+        this.title = "Progression";
         this.bodyCls = "white";
 		this.layout="vbox";
         this.items = [
-		{
-			xtype: "uploadfilemanager",
-			border: false,
-			itemId: "up",
-			flex: 1,
-			width: "100%",
-			uploader: '/upload'
-        },
-		{
-			layout: "hbox",
-			width: "100%",
-			border: false,
-			items: [
 			{
-				flex: 1,
-				border: false
-			},
-			{
-				margin: 10,
-				xtype: "button",
-				itemId: "clickImport",
-				text: "Importer",
-				width: 150
+			
 			}
-			]
-		}
 		];
 		this.callParent(arguments);
 	}
