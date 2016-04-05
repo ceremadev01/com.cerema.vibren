c78a828da4a46286e72d7e6f775c123bfe8043ce
Stéphane Zucatti <stephane.zucatti@me.com>
2016-04-05 09:15:46 +0200

    Update# 93

--- src/Contents/Services/Files.js
+++ src/Contents/Services/Files.js
@@ -70,7 +70,7 @@ Files={
 									}
 								}
 							}).then(function(file) {
-                                console.log(file);
+                                console.log(file.dataValues);
 								var ff=file.dataValues.id;
                                 console.log(ff);
 								vibren.Acquisition.find({where: { id: ff } }).then(function(acq) {
